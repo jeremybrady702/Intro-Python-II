591ec56c7a415b51c31762ad99226949a4ae1433
Jeremy Brady <jbdigitalgeek@gmail.com>
2020-01-15 21:04:28 -0800

    basic game running day 1 mvp done

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, room, name="Player 1"):
+        self.name = name
+        self.room = room
--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description, n_to="nothing", e_to="nothing", s_to="nothing", w_to="nothing"):
+        self.name = name
+        self.description = description
